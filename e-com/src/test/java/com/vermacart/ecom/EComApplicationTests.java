d723fc60660d547c5e3799caaa04d3f65118a52e
vermasuraj11011 <vermasuraj008@gmail.com>
2023-06-22 13:58:30 +0530

    BE e-com

---
+++ e-com/src/test/java/com/vermacart/ecom/EComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermacart.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
